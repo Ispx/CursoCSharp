6b409a40d3aec5b72f53558c90383bb1e927df02
Isaque Paixao <isaquespx98@gmail.com>
2022-05-14 09:49:03 -0300

    Aula de inferencia

---
+++ Fundamentos/Inferencia.cs
@@ -0,0 +1,16 @@
+namespace CursoCSharp.Fundamentos;
+
+public class Inferencia
+{
+    public static void Executar()
+    {
+        var idade = 21;
+        var nome = "Maria";
+        var ehVerdadeiro = true;
+        Console.WriteLine("Idade: " + idade);
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("É verdadeiro': " + ehVerdadeiro);
+
+
+    }
+}
